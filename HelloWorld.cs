8d332599705b2dba9785ce4136be63e45785635c
cslearn1 <140393682+cslearn1@users.noreply.github.com>
2023-07-24 15:22:44 +0500

    Add files via upload

---
+++ HelloWorld.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour
+{
+    void Awake()
+    {
+        print("hello, World!");
+    }
+
+    void Update()
+    {
+        
+    }
+}
